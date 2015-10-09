1ff6f1ac77a4d39b13cffd448456cc7ac90dc3b5
Michael Andorfer <mandorfer.mmt-b2014@fh-salzburg.ac.at>
2015-10-09 10:02:40 +0200

    add constraints and calculations for week day plus calendar week

--- Date Calculator/ViewController.swift
+++ Date Calculator/ViewController.swift
@@ -19,7 +19,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet var datePicker : UIDatePicker!
+    @IBOutlet var kalenderWoche : UILabel!
+    @IBOutlet var wochenTag : UILabel!
+    @IBOutlet var differenzInTagen : UILabel!
+    
+    @IBAction func datePickerValueChanged(datePicker : UIDatePicker!) {
+        
+        // Get necessary information from date picker
+        let datePickerDate = datePicker.date;
+        let currenCalendar = NSCalendar.currentCalendar()
+        let calendarWeek = currenCalendar.component(NSCalendarUnit.WeekOfYear, fromDate: datePickerDate)
+        let weekDay = datePickerDate.weekdayName;
+        
+        // Create week day label text
+        var weekDayStr = "Wochentag : \(weekDay)"
+        
+        // Change label to correct week day
+        wochenTag.text = weekDayStr;
+        
+        // Create calendar week label text
+        var calendarWeekStr = "Kalenderwoche : \(calendarWeek)"
+        
+        // Change label to correct calendar week
+        kalenderWoche.text = calendarWeekStr
+        
 
+        
+        
+    }
+}
 
+// Extension of NSDate which calculates weekday as string
+extension NSDate {
+    // returns weekday name (Sunday-Saturday) as String
+    var weekdayName: String {
+        let formatter = NSDateFormatter();
+        formatter.dateFormat = "EEEE"
+        return formatter.stringFromDate(self)
+    }
 }
 
